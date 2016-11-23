e6cc3686983c9973b0efa8a05100c747c0bdff0b
Imab Asghar <iasghar@folio3.com>
2016-11-23 09:50:51 +0500

    Second Session: - Fetching users through HTTP GET - Show Loader while fetching users - sliding items with remove button - alert before removing - Toast after removing

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -1,32 +1,70 @@
-import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {Http} from '@angular/http'
+import {NavController, LoadingController, AlertController, ToastController} from 'ionic-angular';
 import {User} from "../../models/user";
 import {UserDetailsPage} from "../details/details";
-
+import 'rxjs/add/operator/delay'
 @Component({
   selector: 'page-users',
   templateUrl: 'users.html'
 })
 export class UsersPage {
 
-  users: Array<User> = [];
-  constructor(public navCtrl: NavController) {
-    let user1 = new User();
-    user1.userId = 1;
-    user1.userName = 'Ali';
-    user1.designation = 'SE';
+  users:Array<User> = [];
 
-    let user2 = new User();
-    user2.userId = 2;
-    user2.userName = 'Aun';
-    user2.designation = 'LSE';
+  constructor(public navCtrl:NavController,
+              private http:Http,
+              private toastCtrl: ToastController,
+              private alertCtrl:AlertController,
+              private loadingCtrl:LoadingController) {
 
-    this.users.push(user1);
-    this.users.push(user2);
   }
 
-  showUserDetails(user: User) {
+  ionViewDidEnter() {
+    let loader = this.loadingCtrl.create({
+      content: 'Fetching Users'
+    })
+    loader.present();
+    this.http.get('users.json').delay(2000)
+      .subscribe(result => {
+        this.users = result.json();
+        loader.dismiss();
+      });
+  }
+
+  showDeleteAlert(user) {
+    let alert = this.alertCtrl.create({
+      title: 'Delete User',
+      content: 'Are you sure you want to delete this user',
+      buttons: [{
+        text: 'No',
+        handler: () => {
+          console.log("No");
+        }
+      },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.deleteUser(user);
+          }
+        }]
+    })
+    alert.present();
+  }
+
+  deleteUser(user) {
+    let toast = this.toastCtrl.create({
+      message: 'User has been succesfully deleted',
+      position: 'middle',
+      duration: 3000, 
+      showCloseButton: true
+    })
+    let index = this.users.indexOf(user);
+    this.users.splice(index, 1);
+    toast.present();
+  }
+
+  showUserDetails(user:User) {
     this.navCtrl.push(UserDetailsPage, {user: user})
   }
 
